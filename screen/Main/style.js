0c5995d1730d6a7d6803cb4105514b6030bd70c4
hk22 <wlsgur3710@naver.com>
2022-09-08 12:23:50 +0900

    calendar modal connect

--- screen/Main/style.js
+++ screen/Main/style.js
@@ -122,7 +122,64 @@ const styles = StyleSheet.create({
         fontWeight:'bold'
     },
     ////////////////BottomSheet_Main style//////////////////////////////
-
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        height:500, 
+        width: 370,
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      button: {
+        
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+      },
+      buttonOpen: {
+        backgroundColor: "#F194FF",
+      },
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      exitlogo: {
+        width: 25,
+        height: 25,
+      },
+      facelogo: {
+        width: 35,
+        height: 35,
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center"
+      },
+      calendar:{
+        width: 350,
+        height: 350,
+        marginTop: 50
+      },
+      //calendar
 })
 
 
