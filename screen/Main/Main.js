d63ce5ba4c7ce245b3cc4d11251841be1bfaef52
ReenactmentPark <jaeyeon7531@gmail.com>
2022-09-18 02:19:07 +0900

    메인에 모달 불러오기

--- screen/Main/Main.js
+++ screen/Main/Main.js
@@ -12,103 +12,41 @@ import {
 import { useNavigation,useIsFocused } from '@react-navigation/native';
 import { MMKV } from 'react-native-mmkv'
 import BottomSheet_Main from './BottomSheet_Main';
-import { Calendar } from "react-native-calendars";
+import CalendarModal from './CalenderModal'
+import SeedModal from './SeedModal'
 import styles from './style';
 
 export const storage = new MMKV()
 
 function Main(){
-    if(storage.getString('user')===undefined){  //user정보 캐싱되지 않았다면 서버 통해서 user정보 return,캐싱
-        const user = {
-            userName: '박재연',
-            email: 'jaeyeon7531@gmail.com',
-            phoneNumber: '010-7151-1918',
-            pw:'123456',
-            profileImage:'https://image.fnnews.com/resource/media/image/2022/07/16/202207160834208420_l.jpg',
-            point:0,
-            recycle:0
-            //
-            }      
-        storage.set('user', JSON.stringify(user))
-    }
-    const isFocused = useIsFocused();
-    const screenHeight = Dimensions.get("screen").height;   //phone 높이,폭 px
-    const screenWidth = Dimensions.get("screen").width;
-    const navigation = useNavigation()
-    const jsonUser = storage.getString('user') // { 'userName': '박재연', 'point': 0 }
-    const userObject = JSON.parse(jsonUser)
-    const [modalVisible,setModalVisible]=useState(false)
-    console.log(userObject)
-    console.log(jsonUser)
-    useEffect(() => {}, [isFocused]);
-    const [calendarsmodalVisible, setcalendarModalVisible] = useState(false);
-    const success =[
-      "2022-08-01", "2022-08-14"
-    ];
-        
-      return(
-        <>
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={calendarsmodalVisible}
-        onRequestClose={() => {
-          setcalendarModalVisible(false);
-        }}
-      > 
-      <Pressable style={styles.centeredView}
-      onPress={() => setcalendarModalVisible(false)}
-      >
-        <View style={styles.modalView}>           
-            <Image 
-              style={styles.exitlogo}
-              source={require('../../imageResource/icon/ic_close.png')}
-            />
-            <Calendar
-                style={styles.calendar}
-                theme={{
-                monthTextColor: 'black',
-                arrowColor: '#d1e5cd',
-                textDayFontSize: 16,
-                textMonthFontSize: 30,
-                textDayHeaderFontSize: 0
-              }}
-
-              dayComponent={({date, state}) => {
-                
-                if (success.includes(date.dateString)){
-                  var dateimg = date.dateString.replace(/\-/g,"")
-                  if((dateimg*1)%2==0){
-                    return(
-                      <Image
-                        style={styles.facelogo}
-                        source={require('../../imageResource/icon/ic_face_01.png')}
-                      />
-                    )
-                  }
-                  else{
-                    return(
-                      <Image 
-                        style={styles.facelogo}
-                        source={require('../../imageResource/icon/ic_face_02.png')}
-                      />
-                    )
-                  }
-                }
-                else{
-                  return(
-                    <View>
-                      <Text style={{textAlign: 'center', color: state === 'disabled' ? 'white' : 'black'}}>
-                        {date.day}
-                      </Text>
-                    </View>
-                  )
-                }
-              }}
-            /> 
-        </View>
-      </Pressable>
-    </Modal>
+  if(storage.getString('user')===undefined){  //user정보 캐싱되지 않았다면 서버 통해서 user정보 return,캐싱
+      const user = {
+          userName: '박재연',
+          email: 'jaeyeon7531@gmail.com',
+          phoneNumber: '010-7151-1918',
+          pw:'123456',
+          profileImage:'https://image.fnnews.com/resource/media/image/2022/07/16/202207160834208420_l.jpg',
+          point:0,
+          recycle:0
+          //
+          }      
+      storage.set('user', JSON.stringify(user))
+  }
+  const isFocused = useIsFocused();
+  const screenHeight = Dimensions.get("screen").height;   //phone 높이,폭 px
+  const screenWidth = Dimensions.get("screen").width;
+  const navigation = useNavigation()
+  const jsonUser = storage.getString('user') // { 'userName': '박재연', 'point': 0 }
+  const userObject = JSON.parse(jsonUser)
+  const [modalVisible,setModalVisible]=useState(false)
+  useEffect(() => {}, [isFocused]);
+  const [calendarModalVisible, setCalendarModalVisible] = useState(false);
+  const [seedModalVisible,setSeedModalVisible] = useState(true)
+  const success =[
+    "2022-08-01", "2022-08-14"
+  ];
+    return(
+      <>
         <View style={{flex:1}}>
             <ImageBackground 
                 style={{
@@ -139,7 +77,7 @@ function Main(){
                         <View style={styles.calenderAndNoticeBoxContainer}>
                             <View style={styles.calenderAndNoticeBox}>
                                 <TouchableOpacity
-                                    onPress={()=>setcalendarModalVisible(true)}
+                                    onPress={()=>setCalendarModalVisible(true)}
                                 >
                                     <Image 
                                         style={{marginRight:'15%'}}
@@ -164,13 +102,21 @@ function Main(){
                     </View>
                 </View>
                 <BottomSheet_Main
-                    modalVisible={modalVisible}
-                    setModalVisible={setModalVisible}
+                  modalVisible={modalVisible}
+                  setModalVisible={setModalVisible}
+                />
+                <CalendarModal
+                  calendarModalVisible={calendarModalVisible}
+                  setCalendarModalVisible={setCalendarModalVisible}
+                />
+                <SeedModal
+                  seedModalVisible={seedModalVisible}
+                  setSeedModalVisible={setSeedModalVisible}
                 />
             </ImageBackground>
         </View>
-        </>
-    )
+      </>
+  )
 }
 
 export default Main
