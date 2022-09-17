f3cfb9acc774146c97cd80df0a39408d7d8db747
ReenactmentPark <jaeyeon7531@gmail.com>
2022-09-18 02:18:04 +0900

    달력 모달 옮김
    메인에 있던 달력 모달 CalendarModal.js로 코드 옮김

---
+++ screen/Main/CalenderModal.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useRef } from 'react';
+import {
+    View,
+    Text,
+    Modal,
+    Pressable,
+    Image
+} from 'react-native';
+import { Calendar } from "react-native-calendars";
+import styles from './style'
+
+const CalendarModal=(props)=>{
+    const { calendarModalVisible, setCalendarModalVisible,text} = props;
+    const success = [
+        "2022-08-01", "2022-08-14"
+    ];
+    return(
+        <>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={calendarModalVisible}
+                onRequestClose={() => {
+                    setCalendarModalVisible(false);
+                }}
+            > 
+                <Pressable style={styles.centeredView}
+                    onPress={() => setCalendarModalVisible(false)}
+                >
+                    <View style={[styles.modalView,{alignItems:'center'}]}>           
+                        <Image 
+                            style={styles.exitlogo}
+                            source={require('../../imageResource/icon/ic_close.png')}
+                        />
+                        <Calendar
+                            style={styles.calendar}
+                            theme={{
+                                monthTextColor: 'black',
+                                arrowColor: '#d1e5cd',
+                                textDayFontSize: 16,
+                                textMonthFontSize: 30,
+                                textDayHeaderFontSize: 0
+                            }}
+
+                            dayComponent={({date, state}) => {
+                                
+                                if (success.includes(date.dateString)){
+                                var dateimg = date.dateString.replace(/\-/g,"")
+                                if((dateimg*1)%2==0){
+                                    return(
+                                        <Image
+                                            style={styles.facelogo}
+                                            source={require('../../imageResource/icon/ic_face_01.png')}
+                                        />
+                                    )
+                                }
+                                else{
+                                    return(
+                                        <Image 
+                                            style={styles.facelogo}
+                                            source={require('../../imageResource/icon/ic_face_02.png')}
+                                        />
+                                    )
+                                }
+                                }
+                                else{
+                                    return(
+                                        <View>
+                                            <Text style={{textAlign: 'center', color: state === 'disabled' ? 'white' : 'black'}}>
+                                                {date.day}
+                                            </Text>
+                                        </View>
+                                    )
+                                }
+                            }}
+                        /> 
+                    </View>
+                </Pressable>
+            </Modal>
+        </>
+    )
+}
+export default CalendarModal
