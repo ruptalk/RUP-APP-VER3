8c8bfb511a37d1ff3945947c594ca5425d1efe6a
hk22 <wlsgur3710@naver.com>
2022-11-03 10:01:19 +0900

    랜덤씨앗 생성

--- screen/Main/Main.js
+++ screen/Main/Main.js
@@ -106,33 +106,114 @@ function Main(){
         uri4: require('../../imageResource/flower/flowerA/flowerA_4.gif'),
         uri5: require('../../imageResource/flower/flowerA/flowerA_5.gif')
     },
-]
+    
+    {
+        flowername: 'flowerB',
+        uri1: require('../../imageResource/flower/flowerB/flowerB_1.gif'),
+        uri2: require('../../imageResource/flower/flowerB/flowerB_2.gif'),
+        uri3: require('../../imageResource/flower/flowerB/flowerB_3.gif'),
+        uri4: require('../../imageResource/flower/flowerB/flowerB_4.gif'),
+        uri5: require('../../imageResource/flower/flowerB/flowerB_5.gif')
+    },
+    
+    {
+        flowername: 'flowerC',
+        uri1: require('../../imageResource/flower/flowerC/flowerC_1.gif'),
+        uri2: require('../../imageResource/flower/flowerC/flowerC_2.gif'),
+        uri3: require('../../imageResource/flower/flowerC/flowerC_3.gif'),
+        uri4: require('../../imageResource/flower/flowerC/flowerC_4.gif'),
+        uri5: require('../../imageResource/flower/flowerC/flowerC_5.gif')
+    },
+    
+    {
+        flowername: 'flowerD',
+        uri1: require('../../imageResource/flower/flowerD/flowerD_1.gif'),
+        uri2: require('../../imageResource/flower/flowerD/flowerD_2.gif'),
+        uri3: require('../../imageResource/flower/flowerD/flowerD_3.gif'),
+        uri4: require('../../imageResource/flower/flowerD/flowerD_4.gif'),
+        uri5: require('../../imageResource/flower/flowerD/flowerD_5.gif')
+    },
+    
+    {
+        flowername: 'flowerE',
+        uri1: require('../../imageResource/flower/flowerE/flowerE_1.gif'),
+        uri2: require('../../imageResource/flower/flowerE/flowerE_2.gif'),
+        uri3: require('../../imageResource/flower/flowerE/flowerE_3.gif'),
+        uri4: require('../../imageResource/flower/flowerE/flowerE_4.gif'),
+        uri5: require('../../imageResource/flower/flowerE/flowerE_5.gif')
+    },
+    
+    {
+        flowername: 'flowerF',
+        uri1: require('../../imageResource/flower/flowerF/flowerF_1.gif'),
+        uri2: require('../../imageResource/flower/flowerF/flowerF_2.gif'),
+        uri3: require('../../imageResource/flower/flowerF/flowerF_3.gif'),
+        uri4: require('../../imageResource/flower/flowerF/flowerF_4.gif'),
+        uri5: require('../../imageResource/flower/flowerF/flowerF_5.gif')
+    },
+    
+    {
+        flowername: 'flowerG',
+        uri1: require('../../imageResource/flower/flowerG/flowerG_1.gif'),
+        uri2: require('../../imageResource/flower/flowerG/flowerG_2.gif'),
+        uri3: require('../../imageResource/flower/flowerG/flowerG_3.gif'),
+        uri4: require('../../imageResource/flower/flowerG/flowerG_4.gif'),
+        uri5: require('../../imageResource/flower/flowerG/flowerG_5.gif')
+    },
+    
+    {
+        flowername: 'flowerH',
+        uri1: require('../../imageResource/flower/flowerH/flowerH_1.gif'),
+        uri2: require('../../imageResource/flower/flowerH/flowerH_2.gif'),
+        uri3: require('../../imageResource/flower/flowerH/flowerH_3.gif'),
+        uri4: require('../../imageResource/flower/flowerH/flowerH_4.gif'),
+        uri5: require('../../imageResource/flower/flowerH/flowerH_5.gif')
+    },
+    
+    {
+        flowername: 'flowerI',
+        uri1: require('../../imageResource/flower/flowerI/flowerI_1.gif'),
+        uri2: require('../../imageResource/flower/flowerI/flowerI_2.gif'),
+        uri3: require('../../imageResource/flower/flowerI/flowerI_3.gif'),
+        uri4: require('../../imageResource/flower/flowerI/flowerI_4.gif'),
+        uri5: require('../../imageResource/flower/flowerI/flowerI_5.gif')
+    },
+    {
+        flowername: 'flowerJ',
+        uri1: require('../../imageResource/flower/flowerJ/flowerJ_1.gif'),
+        uri2: require('../../imageResource/flower/flowerJ/flowerJ_2.gif'),
+        uri3: require('../../imageResource/flower/flowerJ/flowerJ_3.gif'),
+        uri4: require('../../imageResource/flower/flowerJ/flowerJ_4.gif'),
+        uri5: require('../../imageResource/flower/flowerJ/flowerJ_5.gif')
+    }
+  ]
 
 const FlowerGIF =()=>{
   console.log(seedColor,"좋아요")
+  var tmp = ''
   switch(seedColor){
-     
       case 'Pink':
-        var tmp = flower[0].uri2
-          
+        tmp = flower[0].uri1
+        break;
       case 'Brown':
-          
+        break;
       case 'Lavender':
-          
+        break; 
       case 'Green':
-          
+        break;
       case 'Purple':
-         
+         break;
       case 'Yellow':
-      
-      defalut:
-        return (
-          <Image 
-            source={tmp} 
-            style={{width:300, height:400, marginLeft:5}}
-          />
-      )
+        tmp = flower[Math.floor(Math.random()*5)+5].uri1
+        break;
   }
+  return (
+    <Image 
+      source={tmp} 
+      style={{width:300, height:400, marginLeft:5}}
+    />
+  )
+
 }
 
   
