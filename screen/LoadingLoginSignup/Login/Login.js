dec9ea79f6d2536df3daf959949f884a55077dbc
ReenactmentPark <jaeyeon7531@gmail.com>
2022-12-07 17:14:24 +0900

    비번찾기 페이지 들어갔다가 나왔을 때 로그인바텀시트 자동으로 오픈

--- screen/LoadingLoginSignup/Login/Login.js
+++ screen/LoadingLoginSignup/Login/Login.js
@@ -10,17 +10,31 @@ import {
 } from 'react-native';
 import BottomSheet_login from './BottomSheet_login.js';
 import styles from './style.js';
+import { useIsFocused } from '@react-navigation/native';
+
 
 // import { MMKV } from 'react-native-mmkv'
 // export const storage = new MMKV()
 
 const Login = (props) => {
     // storage.clearAll()
-    const [ modalVisible, setModalVisible ] = useState(true);
-
+    const [ modalVisible, setModalVisible ] = useState(false);
+    const [check,setCheck]=useState(0)
     const pressButton=()=>{
         setModalVisible(true);
     }
+    const isFocused = useIsFocused();
+    useEffect(()=>{                 ///비밀번호를 잊으셨나요 페이지 들어갔다 나와도 로그인모달 올라오게 하는 함수
+        if(check%2===0){
+            setCheck(check+1)
+            setModalVisible(true)
+        }
+        else{
+            setCheck(check+1)
+            setModalVisible(false)
+        }
+    },[isFocused])
+
     return (
         <View style={styles.container}>
             <View style={{marginTop:'25%'}}/>
