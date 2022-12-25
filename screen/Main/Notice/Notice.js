021d53c89d7816e96c1328813cec2dcabd15c779
hke2 <59246907+hke2@users.noreply.github.com>
2022-12-25 20:17:40 +0900

    공지 테스트 중(서버 데이터없음)

--- screen/Main/Notice/Notice.js
+++ screen/Main/Notice/Notice.js
@@ -9,15 +9,53 @@ import {
     Modal
 } from 'react-native'
 import BigList from "react-native-big-list";
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation,useIsFocused} from '@react-navigation/native';
 import styles from './style'
 import Notice_modal from './Notice_modal';
+import axios from 'axios'
+import { MMKV } from 'react-native-mmkv'
+export const storage = new MMKV()
+
+
 function Notice(){
+  const jsonUser = storage.getString('user') 
+  const userObject = JSON.parse(jsonUser)
+  const [uid,]=useState(userObject.uid)
+
+  const [notice,setNotice] = useState([])
   const navigation = useNavigation()
+  const isFocused = useIsFocused()
+
+  useEffect(()=>{
+    getNotice(uid)
+  },[isFocused])
+
+  const getNotice=(uid)=>{
+    axios.get('http://152.67.193.99/home/notice-and-point-record', { //http://152.67.193.99 //http://13.124.80.15
+    params: {
+      uid: uid
+    }
+  })
+  .then(function(response) {
+    console.log(response,"?DFSF?")
+    // const getNotice = {
+    //   uid:response.data.uid,
+    //   noticeDate : response.data.noticeDate,
+    //   notice : response.data.notice,
+    //   pointRecord : response.date.pointRecord      
+    // }
+    // setNotice(getNotice)      
+  })
+  .catch(function (error) {
+    console.log(error);
+    console.log('fail')
+  });
+}
 
   const renderItem = ({item}) => (
       <Item date={item.date} point={item.point}/>
   );
+
   const Item = ({date,point}) => (
     <View>
         <View style={styles.horizonalLine}/>
@@ -27,8 +65,10 @@ function Notice(){
         </View>
     </View>
   );
+
   const [noticemodalVisible, setnoticeModalVisible] = useState(false);
   const [text, setText]=useState('공지사항')
+
   return(
     <>
       <Notice_modal
@@ -44,17 +84,19 @@ function Notice(){
         >
           <Image source={require('../../../imageResource/icon/ic_arrow_left.png')}/>
         </TouchableOpacity>
+
         <View style={styles.horizonalLine}/>
-        <TouchableOpacity
-          style={{marginTop:'5%',marginLeft:30,flexDirection:'row',alignItems:'center'}}
-          onPress={()=>setnoticeModalVisible(true)}
-        >
+          <TouchableOpacity
+            style={{marginTop:'5%',marginLeft:30,flexDirection:'row',alignItems:'center'}}
+            onPress={()=>setnoticeModalVisible(true)}
+          >
           <Image style={{width:20,height:20}} source={require('../../../imageResource/icon/ic_notice_02.png')}/>
           <Text style={{marginLeft:'5%',fontWeight:'bold'}}>[공지사항] v 1.1 업데이트 되었습니다.</Text>
         </TouchableOpacity>
+        
         <SafeAreaView style={{height:'80%'}}>
           <BigList
-            data={Data}
+            data={notice.pointRecord}
             renderItem={renderItem}
             showsVerticalScrollIndicator={false}
           />
@@ -66,132 +108,132 @@ function Notice(){
 
 export default Notice
 
-const Data = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      date: '22.01.01',
-      point: '1'
-    },
-    {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '2'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '3'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '2'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '4'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '5'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        date: '22.01.01',
-        point: '1'
-      },
+// const Data = [
+//     {
+//       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//       date: '22.01.01',
+//       point: '1'
+//     },
+//     {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '2'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '3'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '2'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '4'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '5'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
+//       {
+//         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//         date: '22.01.01',
+//         point: '1'
+//       },
 
     
-  ];
+//   ];
